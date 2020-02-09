6c262e27cce389ce059292538894e0599868e0b0
anushka <anushka@cs.washington.edu>
2020-02-08 23:48:42 -0800

    adding edited data and line graph code

--- src/index.js
+++ src/index.js
@@ -25,6 +25,23 @@ var svg = d3.select("#display")
 // var data = [ {x:2010, y:10}, {x:2012, y:20}, {x:2005, y:20} ]
 //
 // X scale and Axis
+//
+// // Add 3 dots for 0, 50 and 100%
+// svG
+//   .selectAll("whatever")
+//   .data(data)
+//   .enter()
+//   .append("circle")
+//     .attr("cx", function(d){ return x(d.x) })
+//     .attr("cy", function(d){ return y(d.y) })
+//     .attr("r", 7)
+
+const csvFile = require('./data/pivoted_data.csv');
+d3.csv(csvFile).then(function(theData) {
+  var sumstat = d3.nest() // nest function allows to group the calculation per level of a factor
+    .key(function(d) { return d.Category;})
+    .entries(theData);
+
 var x = d3.scaleLinear()
     .domain([2000, 2020])         // This is the min and the max of the data: 0 to 100 if percentages
     .range([0, width]);       // This is the corresponding value I want in Pixel
@@ -40,30 +57,23 @@ var y = d3.scaleLinear()
 svg
   .append('g')
   .call(d3.axisLeft(y));
-//
-// // Add 3 dots for 0, 50 and 100%
-// svG
-//   .selectAll("whatever")
-//   .data(data)
-//   .enter()
-//   .append("circle")
-//     .attr("cx", function(d){ return x(d.x) })
-//     .attr("cy", function(d){ return y(d.y) })
-//     .attr("r", 7)
 
-const csvFile = require('./data/federal_spending_2020_2024.csv');
-d3.csv(csvFile).then(function(theData) {
-	//console.log(theData[0]);
-    var circles = svg.selectAll(".dot")
-        .data(theData)
-        .enter()
-		.append("circle")
-        // .on("mouseover", tip.show)
-        // .on("mouseout", tip.hide);
-	var circlesAttr = circles
-	    .attr("class", "dot")
-        .attr("cx", function(d) {  return x(d.Year); })
-        // .attr("width", x.bandwidth())
-        .attr("cy", function(d) {console.log(y(d.GDP)); return y(d.Pensions); })
-		.attr("r", 5)
+var res = sumstat.map(function(d){ return d.key }) // list of group names
+  var color = d3.scaleOrdinal()
+    .domain(res)
+    .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#f7c82d','#a65628','#f781bf','#999999', '#4287f5'])
+
+svg.selectAll(".line")
+      .data(sumstat)
+      .enter()
+      .append("path")
+        .attr("fill", "none")
+        .attr("stroke", function(d){ return color(d.key) })
+        .attr("stroke-width", 1.5)
+        .attr("d", function(d){
+          return d3.line()
+            .x(function(d) { return x(d.Year); })
+            .y(function(d) { return y(d.Spending); })
+            (d.values)
+            })
 });
