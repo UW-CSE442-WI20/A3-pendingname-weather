210dda3763e8cb11b3b1499a8281064f6bf6ee90
joon <joon0025@cs.washington.edu>
2020-02-09 18:58:26 -0800

    Merge branch 'master' of github.com:UW-CSE442-WI20/A3-pendingname-weather
    # Conflicts:
    #	src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // set the dimensions and margins of the graph
-var margin = {top: 20, right: 60, bottom: 30, left: 60},
+var margin = {top: 20, right: 80, bottom: 30, left: 60},
     width = 800 - margin.left - margin.right,
     height = 600 - margin.top - margin.bottom;
 
@@ -81,3 +81,56 @@ d3.csv(csvFile).then(function(theData) {
         .y1(function(d) { return y(d[1]); })
       )
 });
+
+// Handmade legend
+svg.append("circle").attr("cx",20).attr("cy",50).attr("r", 6).style("fill", "#69b3a2")
+svg.append("circle").attr("cx",20).attr("cy",70).attr("r", 6).style("fill", "#69b3a2")
+svg.append("circle").attr("cx",20).attr("cy",90).attr("r", 6).style("fill", "#69b3a2")
+svg.append("circle").attr("cx",20).attr("cy",110).attr("r", 6).style("fill", "#69b3a2")
+svg.append("circle").attr("cx",20).attr("cy",130).attr("r", 6).style("fill", "#69b3a2")
+svg.append("circle").attr("cx",20).attr("cy",150).attr("r", 6).style("fill", "#69b3a2")
+svg.append("circle").attr("cx",20).attr("cy",170).attr("r", 6).style("fill", "#69b3a2")
+svg.append("circle").attr("cx",20).attr("cy",190).attr("r", 6).style("fill", "#69b3a2")
+svg.append("circle").attr("cx",20).attr("cy",210).attr("r", 6).style("fill", "#69b3a2")
+svg.append("circle").attr("cx",20).attr("cy",230).attr("r", 6).style("fill", "#69b3a2")
+
+svg.append("text").attr("x", 30).attr("y", 50).text("Welfare").style("font-size", "15px").attr("alignment-baseline","middle")
+svg.append("text").attr("x", 30).attr("y", 70).text("Transportation").style("font-size", "15px").attr("alignment-baseline","middle")
+svg.append("text").attr("x", 30).attr("y", 90).text("Protection").style("font-size", "15px").attr("alignment-baseline","middle")
+svg.append("text").attr("x", 30).attr("y", 110).text("Pensions").style("font-size", "15px").attr("alignment-baseline","middle")
+svg.append("text").attr("x", 30).attr("y", 130).text("Other Spending").style("font-size", "15px").attr("alignment-baseline","middle")
+svg.append("text").attr("x", 30).attr("y", 150).text("Interest").style("font-size", "15px").attr("alignment-baseline","middle")
+svg.append("text").attr("x", 30).attr("y", 170).text("Health Care").style("font-size", "15px").attr("alignment-baseline","middle")
+svg.append("text").attr("x", 30).attr("y", 190).text("General Government").style("font-size", "15px").attr("alignment-baseline","middle")
+svg.append("text").attr("x", 30).attr("y", 210).text("Education").style("font-size", "15px").attr("alignment-baseline","middle")
+svg.append("text").attr("x", 30).attr("y", 230).text("Defense").style("font-size", "15px").attr("alignment-baseline","middle")
+
+//
+// Add one dot in the legend for each name.
+
+// var size = 20
+// svg.selectAll("myrect")
+//     .data(theData)
+//     .enter()
+//     .append("rect")
+//     .attr("x", 400)
+//     .attr("y", function(d,i){ return 10 + i*(size+5)}) // 100 is where the first dot appears. 25 is the distance between dots
+//     .attr("width", size)
+//     .attr("height", size)
+//     .style("fill", function(d){ return color(d)})
+//     .on("mouseover", highlight)
+//     .on("mouseleave", noHighlight)
+//
+// // Add one dot in the legend for each name.
+// svg.selectAll("mylabels")
+//     .data(theData)
+//     .enter()
+//     .append("text")
+//     .attr("x", 400 + size*1.2)
+//     .attr("y", function(d,i){ return 10 + i*(size+5) + (size/2)}) // 100 is where the first dot appears. 25 is the distance between dots
+//     .style("fill", function(d){ return color(d)})
+//     .text(function(d){ return d})
+//     .attr("text-anchor", "left")
+//     .style("alignment-baseline", "middle")
+//     .on("mouseover", highlight)
+//     .on("mouseleave", noHighlight);
